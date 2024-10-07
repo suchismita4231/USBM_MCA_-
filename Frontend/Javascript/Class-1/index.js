e488de86843d4ead015b40b03d81105d25765eb4
suchismita4231 <suchismitamohanty1792002@gmail.com>
2024-10-07 12:17:54 +0530

    string task added

---
+++ Frontend/Javascript/Class-1/index.js
@@ -0,0 +1,66 @@
+// let a = 25;
+// let b= 43;
+// console.log(a+b);
+
+//Undefined
+
+// let data;
+// console.log(typeof data);
+
+//Number
+// let num1 = 45;
+// let decimal_no = 1.001;
+// let pi = 3.14;
+// let roll_no = 22;
+
+// console.log(typeof num1);
+
+// //Addition
+
+// let num2 = 100;
+// let num3 = 25;
+// let addition = num2 +num3;
+// let substraction = num2 - num3;
+// let multiplication = num2 * num3;
+// let division = num2 / num3;
+
+// console.log(addition);
+// console.log(substraction);
+// console.log(multiplication);
+// console.log(division);
+
+// //todo addition , substraction ,multiplication and division the push it into your github account . once you done share me the link in the group.
+
+
+// //BigInt
+// let huge_no =  13447758768798898088980980990.2345567890088776665555;
+// console.log(typeof huge_no);
+
+// //NaN(Not a number)
+
+// let num5 = 0;
+// let num6 = 0;
+// console.log(num5 / num6);
+
+
+let num7 = 10;
+let num8 = 0;
+console.log(num7/num8);
+
+
+//Boolean: decission making of programming
+
+let isMarried = false;
+let isSingle = true;
+console.log(typeof isMarried);
+console.log(typeof isSingle);
+console.log(isMarried);
+console.log(isSingle);
+
+let name = "Suchi";
+let age = 22;
+let city = "Bhubaneswar";
+let rollno = 136;
+let college = "USBM";
+let batch = "2023-25";
+console.log(`my name is ${name} from ${city} , my rollno is ${rollno} , age is ${age} , college is ${college} , batch is ${batch}`);
